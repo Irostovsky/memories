f1ed56b2a9c320ca9b40c0d9226c7b10c3ffaef7
Ivan Rostovsky <ir@irostovsky.local>
2012-12-12 15:06:30 +0200

    Fixed for couchrest_model 1.1.3

--- lib/memories/base.rb
+++ lib/memories/base.rb
@@ -3,9 +3,9 @@ module Memories
   def self.included(base)
     base.property(:milestone_memories) do |milestone_memory|
       milestone_memory.property :version
-      milestone_memory.property :annotations, 
-        Memories::Annotation, 
-        :init_method => proc { |value| 
+      milestone_memory.property :annotations,
+        Memories::Annotation,
+        :init_method => proc { |value|
           a = Memories::Annotation.new
           value.keys.each do |key|
             a.send key, value[key]
@@ -18,11 +18,11 @@ module Memories
     base.after_save :decode_attachments
     base.send :extend, ClassMethods
   end
-  
+
   module ClassMethods #:nodoc: all
     # If you'd like to exclude certain properties from versioning, simply pass those properties
-    # to this method: 
-    #   
+    # to this method:
+    #
     #   class MyDocument < CouchRest::Model::Base
     #     use_database MY_DATABASE
     #     include Memories
@@ -41,8 +41,8 @@ module Memories
 
 
     # If you'd like to explicitly define which properties you want versioned simply pass those properties
-    # to this method: 
-    #   
+    # to this method:
+    #
     #   class MyDocument < CouchRest::Model::Base
     #     use_database MY_DATABASE
     #     include Memories
@@ -58,11 +58,11 @@ module Memories
       @remember_called = true
       props = props.map {|p| p.to_s}
       if self.remember_properties.nil?
-        self.remember_properties = props 
+        self.remember_properties = props
       else
-        self.remember_properties += props 
+        self.remember_properties += props
       end
-    end 
+    end
 
     # Returns true if self is set up to remember attachments. False otherwise.
     def remember_attachments? #:doc:
@@ -70,36 +70,36 @@ module Memories
     end
 
     # Returns a list of attachment patterns for versioning. The list may contain
-    # strings, denoting the names of attachments to version, but it may 
+    # strings, denoting the names of attachments to version, but it may
     # also contain regular expressions, indicating that attachments
     # with matching names should be versioned.
     def remember_attachments; @remember_attachments || []; end
 
-    # If you'd like to version attachments, simply call this macro in your 
-    # class definition: 
+    # If you'd like to version attachments, simply call this macro in your
+    # class definition:
     #
     #   class MyDoc < CouchRest::Model::Base
     #     use_database MY_DB
     #     include Memories
-    # 
+    #
     #     remember_attachments!
     #   end
     #
-    # If you only want specific attachments versioned, pass 
+    # If you only want specific attachments versioned, pass
     # strings and/or regular expressions to this macro. Any attachments
     # with matching names will be versioned.
     #
     #   class HtmlPage < CouchRest::Model::Base
     #     use_database MY_DB
     #     include Memories
-    # 
+    #
     #     remember_attachments! "image.png", %r{stylesheets/.*}
     #   end
     #
     def remember_attachments!(*attachment_names) #:doc:
       if attachment_names.empty?
         @remember_attachments = [/.*/]
-      else 
+      else
         @remember_attachments = attachment_names
       end
     end
@@ -120,35 +120,35 @@ module Memories
       @forget_properties = props
     end
   end
-   
+
   VERSION_REGEX = /(?:rev-)?(\d+)-[a-zA-Z0-9]+/ #:nodoc:
-  
+
   # Returns a list of attachments it should remember.
   def attachments_to_remember
     return [] unless self.class.remember_attachments?
-    (self.database.get(self.id)["_attachments"] || {}).keys.reject do |a| 
-      a.match(VERSION_REGEX) || 
+    (self.database.get(self.id)["_attachments"] || {}).keys.reject do |a|
+      a.match(VERSION_REGEX) ||
         !(self.class.remember_attachments.map { |attachment_name_pattern|
           a.match attachment_name_pattern
         }.inject(false) {|b, sum| sum || b})
-    end 
+    end
   end
 
   # Returns a list of attachments it should not version
   def attachments_to_forget
     return [] unless self.class.remember_attachments?
-    (self.database.get(self.id)["_attachments"] || {}).keys.reject do |a| 
-      a.match(VERSION_REGEX) || 
+    (self.database.get(self.id)["_attachments"] || {}).keys.reject do |a|
+      a.match(VERSION_REGEX) ||
         (self.class.remember_attachments.map { |attachment_name_pattern|
           a.match attachment_name_pattern
         }.inject(false) {|b, sum| sum || b})
-    end 
+    end
   end
-  
-  # Revert the document to a specific version and save. 
+
+  # Revert the document to a specific version and save.
   # You can provide either a complete revision number ("1-u54abz3948302sjjej3jej300rj", or "rev-1-u54abz3948302sjjej3jej300rj")
   # or simply a number (e.g, 1, 4, 100, etc.).
-  #   my_doc.revert_to! 3 # ==> would revert your document "my_doc" to version 3. 
+  #   my_doc.revert_to! 3 # ==> would revert your document "my_doc" to version 3.
   def revert_to!(version)
     revert version, :hard
     self
@@ -172,20 +172,20 @@ module Memories
   end
 
   # Revert to a given milestone and save. Milestones are stored in the .milestones array.
-  # Reverting to milestone "n" reverts to milestone represented in the nth element in the 
+  # Reverting to milestone "n" reverts to milestone represented in the nth element in the
   # milestones array.
   def revert_to_milestone!(n)
     verify_milestone_exists n
     self.revert_to! self.milestones[n.to_i-1].version
   end
- 
+
   # Same as #revert_to_milestone!, except it doesn't save.
   def revert_to_milestone(n)
     verify_milestone_exists n
     self.revert_to self.milestones[n.to_i-1].version
   end
- 
-  # Same as #rollback_to_latest_milestone, but doesn't save. 
+
+  # Same as #rollback_to_latest_milestone, but doesn't save.
   def rollback_to_latest_milestone
     self.revert_to_milestone self.milestones.count
   end
@@ -195,15 +195,15 @@ module Memories
   def rollback_to_latest_milestone!
     self.revert_to_milestone! self.milestones.count
   end
- 
-  # Retrieve the entire revision number, given an integer. 
-  # For example, suppose my doc has 5 versions. 
+
+  # Retrieve the entire revision number, given an integer.
+  # For example, suppose my doc has 5 versions.
   #   my_doc.version_id 4 #==> "rev-4-74fj838r838fhjkdfklasdjrieu4839493"
   def version_id(version_num)
     self["_attachments"].keys.select {|a| a.match /^rev-#{version_num}-.*$/}.first if self["_attachments"]
   end
 
-  # Retrieve the version number, given a revision id. 
+  # Retrieve the version number, given a revision id.
   # For example,
   #   my_doc.version_number "rev-5-kjfldsjaiu932489023rewar" #==> 5
   #   my_doc.version_number "4-jkfldsjli3290843029irelajfldsa" # ==> 4
@@ -217,8 +217,8 @@ module Memories
     current_version - 1
   end
 
-  # Returns a simple version number (integer) corresponding to the current revision. 
-  # For example, suppose the current revision (_rev) is: "4-jkfdlsi9432943wklrejwalr94302". 
+  # Returns a simple version number (integer) corresponding to the current revision.
+  # For example, suppose the current revision (_rev) is: "4-jkfdlsi9432943wklrejwalr94302".
   #   my_doc.current_version #==> 4
   def current_version
     version_number rev
@@ -240,7 +240,7 @@ module Memories
   #     notes "Passed first round of editing."
   #     approved_by "Joe the editor."
   #   end
-  # 
+  #
   # You may annotate with whatever properties you desire. "notes" and "approved_by" were simply examples.
   def milestone!(&block)
     annotations = Memories::Annotation.new
@@ -251,12 +251,12 @@ module Memories
 
   #As of version 0.2.0, Memories also supports milestones. Milestones are special versions that you want to flag in some way.
   #For example, suppose you were creating a content management system, and every time someone publishes an article to the website, you want to flag the version
-  #they published as a milestone. 
+  #they published as a milestone.
   #
   #    class Article < CouchRest::Model::Base
   #      include Memories
   #      use_database SOME_DATABASE
-  #      
+  #
   #      property :title
   #      property :author
   #      property :body
@@ -267,22 +267,22 @@ module Memories
   #    end
   #
   #    a = Article.create(
-  #      :title => "Memories gem makes versioning simple", 
-  #      :author => "moonmaster9000", 
+  #      :title => "Memories gem makes versioning simple",
+  #      :author => "moonmaster9000",
   #      :body => <<-ARTICLE
   #        Check it out at http://github.com/moonmaster9000/memories
   #      ARTICLE
   #    )
   #    a.save
   #    a.publish!
-  #    a.current_version #==> 1 
+  #    a.current_version #==> 1
   #    a.milestone! do
   #      name "First publish."
   #      notes "Passed all relevant editing. Signed off by moonmaster10000"
   #    end
   #
   #Notice that we annotated our milestone; we gave it a name, and some notes. You can annotate with whatever properties you desire. The annotation do block is entirely optional.
-  #Now that we've created a milestone, let's inspect it via the `milestones` array: 
+  #Now that we've created a milestone, let's inspect it via the `milestones` array:
   #
   #    a.milestones.count #==> 1
   #    a.milestones.last.version # ==> 1
@@ -297,8 +297,8 @@ module Memories
   #
   #And now our document properties are back to the where they were when we first published the document.
   #
-  #If you want to access the data from a milestone, simply use the "data" method: 
-  #    
+  #If you want to access the data from a milestone, simply use the "data" method:
+  #
   #    a.milestones.first.data.title #==> returns the "title" attribute on the first milestone
   #    a.milestones.each do |m|
   #      puts "Version: " + m.version
@@ -324,7 +324,7 @@ module Memories
   end
 
   # When you soft revert a document, you can ask what the logical revision of it is.
-  # For example, suppose you soft revert a document with 10 versions to version 2. 
+  # For example, suppose you soft revert a document with 10 versions to version 2.
   #     @doc.revert_to 2
   # When you ask the logical revision, you'll receive the revision number of version 2:
   #     @doc.logical_revision #==> 'rev-2-kfdlsa432890432890432'
@@ -333,11 +333,11 @@ module Memories
   #     @doc.rev #==> '11-qwerty1234567890'
   #     @doc.logical_revision #==> 'rev-11-qwerty1234567890'
   def logical_revision
-    @logical_revision || "rev-" + self.rev 
+    @logical_revision || "rev-" + self.rev
   end
 
   # When you soft revert a document, you can ask what the logical version number of it is.
-  # For example, suppose you soft revert a document with 10 versions to version 2. 
+  # For example, suppose you soft revert a document with 10 versions to version 2.
   #   @doc.revert_to 2
   # When you ask the logical version number, you'll receive 2:
   #   @doc.logical_version_number #==> 2
@@ -370,15 +370,15 @@ module Memories
 
   def revert(version, revert_type = :soft)
     raise StandardError, "Unknown revert type passed to 'revert' method. Allowed types: :soft, :hard." if revert_type != :soft && revert_type != :hard
-   
+
     if (match = version.to_s.match(VERSION_REGEX)) && match[1]
-      version = match[1].to_i  
+      version = match[1].to_i
     end
 
     raise StandardError, "Unknown version" unless version.kind_of?(Fixnum)
     raise StandardError, "The requested version does not exist" if version < 1 or version > current_version
     return self if version == current_version
-    
+
     if properties = JSON.parse(self.read_attachment(version_id version))
       revert_attachments properties
       self.update_attributes_without_saving properties
@@ -404,10 +404,10 @@ module Memories
 
   def add_version_attachment
     current_document_version = Attachment.new prep_for_versioning(self.database.get(self.id)).to_json
-   
-    self.create_attachment( 
-      :file => current_document_version, 
-      :content_type => "application/json", 
+
+    self.create_attachment(
+      :file => current_document_version,
+      :content_type => "application/json",
       :name => "rev-#{self.rev}"
     )
   end
@@ -418,7 +418,7 @@ module Memories
     strip_unversioned_properties versioned_doc
     versioned_doc
   end
-  
+
   def add_attachment_memories(doc)
     doc['attachment_memories'] = {
       'versioned_attachments' => base64_encoded_attachments_to_remember(doc),
@@ -431,7 +431,7 @@ module Memories
     attachments_to_remember.each do |a|
       attachment_data = self.read_attachment(a) rescue nil
       if attachment_data
-        encoded_attachments[a] = { 
+        encoded_attachments[a] = {
           :content_type => doc['_attachments'][a]['content_type'],
           :data => Base64.encode64(attachment_data).gsub(/\s/, '')
         }
@@ -442,24 +442,24 @@ module Memories
 
   def strip_unversioned_properties(doc)
     if self.class.remember_properties.nil?
-      doc.delete_if {|k,v| self.class.forget_properties.include? k}
+      doc.to_hash.delete_if {|k,v| self.class.forget_properties.include? k}
     else
-      doc.delete_if {|k,v| !self.class.remember_properties.include?(k)}
-    end   
+      doc.to_hash.delete_if {|k,v| !self.class.remember_properties.include?(k)}
+    end
   end
 
-  # why is this necessary? Because couchrest destructively base64 encodes all attachments in your document. 
+  # why is this necessary? Because couchrest destructively base64 encodes all attachments in your document.
   def decode_attachments
     self["_attachments"].each do |attachment_id, attachment_properties|
-      self["_attachments"][attachment_id]["data"] = Base64.decode64 attachment_properties["data"] if attachment_properties["data"] 
+      self["_attachments"][attachment_id]["data"] = Base64.decode64 attachment_properties["data"] if attachment_properties["data"]
     end if self["_attachments"]
   end
-  
+
   def overwrite_timestamp(properties)
     timestamp = 'updated_at'
     if (properties.keys.include?(timestamp) && old_updated_at_time = Time.parse(properties[timestamp]) rescue nil)
       write_attribute(timestamp, old_updated_at_time)
     end
   end
-  
+
 end
